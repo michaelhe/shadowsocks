96a8ad7ed55b6be9cb1caccfced34774c27f3c5a
Michael <lizicloud@gmail.com>
2017-04-24 15:06:47 +0800

    update manager

--- shadowsocks/manager.py
+++ shadowsocks/manager.py
@@ -95,29 +95,6 @@ class Manager(object):
             logging.error("server not exist at %s:%d" % (config['server'],
                                                          port))
 
-    #def handle_event(self, sock, fd, event):
-    #    if sock == self._control_socket and event == eventloop.POLL_IN:
-    #        data, self._control_client_addr = sock.recvfrom(BUF_SIZE)
-    #        parsed = self._parse_command(data)
-    #        if parsed:
-    #            command, config = parsed
-    #            a_config = self._config.copy()
-    #            if config:
-    #                # let the command override the configuration file
-    #                a_config.update(config)
-    #            if 'server_port' not in a_config:
-    #                logging.error('can not find server_port in config')
-    #            else:
-    #                if command == 'add':
-    #                    self.add_port(a_config)
-    #                    self._send_control_data(b'ok')
-    #                elif command == 'remove':
-    #                    self.remove_port(a_config)
-    #                    self._send_control_data(b'ok')
-    #                elif command == 'ping':
-    #                    self._send_control_data(b'pong')
-    #                else:
-    #                    logging.error('unknown command %s', command)
 
     def _parse_command(self, data):
         # commands:
@@ -150,143 +127,13 @@ class Manager(object):
             r.clear()
         self._statistics.clear()
 
-    #def handle_periodic(self):
-    #    r = {}
-    #    i = 0
-    #
-    #    def send_data(data_dict):
-    #        if data_dict:
-    #            # use compact JSON format (without space)
-    #            data = common.to_bytes(json.dumps(data_dict,
-    #                                              separators=(',', ':')))
-    #            logging.info('stat is : %s' % data)
-    #            self._send_control_data(b'stat: ' + data)
-    #
-    #    for k, v in self._statistics.items():
-    #        r[k] = v
-    #        i += 1
-    #        # split the data into segments that fit in UDP packets
-    #        if i >= STAT_SEND_LIMIT:
-    #            send_data(r)
-    #            r.clear()
-    #            i = 0
-    #    if len(r) > 0:
-    #        send_data(r)
-    #    self._statistics.clear()
-
-    #def _send_control_data(self, data):
-    #    if not self._control_client_addr:
-    #        return
-    #
-    #    try:
-    #        self._control_socket.sendto(data, self._control_client_addr)
-    #    except (socket.error, OSError, IOError) as e:
-    #        error_no = eventloop.errno_from_exception(e)
-    #        if error_no in (errno.EAGAIN, errno.EINPROGRESS,
-    #                        errno.EWOULDBLOCK):
-    #            return
-    #        else:
-    #            shell.print_exception(e)
-    #            if self._config['verbose']:
-    #                traceback.print_exc()
-
     def run(self):
         self._loop.run()
-
 
 def run(config):
     Manager(config).run()
 
 
-def test():
-    import time
-    import threading
-    import struct
-    from shadowsocks import cryptor
-
-    logging.basicConfig(level=5,
-                        format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    enc = []
-    eventloop.TIMEOUT_PRECISION = 1
-
-    def run_server():
-        config = {
-            'server': '127.0.0.1',
-            'local_port': 1081,
-            'port_password': {
-                '8381': 'foobar1',
-                '8382': 'foobar2'
-            },
-            'method': 'aes-256-cfb',
-            'manager_address': '127.0.0.1:6001',
-            'timeout': 60,
-            'fast_open': False,
-            'verbose': 2
-        }
-        manager = Manager(config)
-        enc.append(manager)
-        manager.run()
-
-    t = threading.Thread(target=run_server)
-    t.start()
-    time.sleep(1)
-    manager = enc[0]
-    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    cli.connect(('127.0.0.1', 6001))
-
-    # test add and remove
-    time.sleep(1)
-    cli.send(b'add: {"server_port":7001, "password":"asdfadsfasdf"}')
-    time.sleep(1)
-    assert 7001 in manager._relays
-    data, addr = cli.recvfrom(1506)
-    assert b'ok' in data
-
-    cli.send(b'remove: {"server_port":8381}')
-    time.sleep(1)
-    assert 8381 not in manager._relays
-    data, addr = cli.recvfrom(1506)
-    assert b'ok' in data
-    logging.info('add and remove test passed')
-
-    # test statistics for TCP
-    header = common.pack_addr(b'google.com') + struct.pack('>H', 80)
-    data = cryptor.encrypt_all(b'asdfadsfasdf', 'aes-256-cfb',
-                               header + b'GET /\r\n\r\n')
-    tcp_cli = socket.socket()
-    tcp_cli.connect(('127.0.0.1', 7001))
-    tcp_cli.send(data)
-    tcp_cli.recv(4096)
-    tcp_cli.close()
-
-    data, addr = cli.recvfrom(1506)
-    data = common.to_str(data)
-    assert data.startswith('stat: ')
-    data = data.split('stat:')[1]
-    stats = shell.parse_json_in_str(data)
-    assert '7001' in stats
-    logging.info('TCP statistics test passed')
-
-    # test statistics for UDP
-    header = common.pack_addr(b'127.0.0.1') + struct.pack('>H', 80)
-    data = cryptor.encrypt_all(b'foobar2', 'aes-256-cfb',
-                               header + b'test')
-    udp_cli = socket.socket(type=socket.SOCK_DGRAM)
-    udp_cli.sendto(data, ('127.0.0.1', 8382))
-    tcp_cli.close()
-
-    data, addr = cli.recvfrom(1506)
-    data = common.to_str(data)
-    assert data.startswith('stat: ')
-    data = data.split('stat:')[1]
-    stats = json.loads(data)
-    assert '8382' in stats
-    logging.info('UDP statistics test passed')
-
-    manager._loop.stop()
-    t.join()
-
 
 if __name__ == '__main__':
-    test()
+    pass
